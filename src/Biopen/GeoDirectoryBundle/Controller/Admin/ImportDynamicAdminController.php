91c8dd099ac6d838470efad24dc5c9fc6863dc95
Sebastian Castro <90scastro@gmail.com>
2019-01-11 19:15:48 +0100

    Remove no more used code

--- src/Biopen/GeoDirectoryBundle/Controller/Admin/ImportDynamicAdminController.php
+++ src/Biopen/GeoDirectoryBundle/Controller/Admin/ImportDynamicAdminController.php
@@ -16,30 +16,7 @@ class ImportDynamicAdminController extends Controller
         $this->addFlash('sonata_flash_success', "Les éléments sont en cours d'importation. Cela peut prendre plusieurs minutes.");
 
         // $dataToImport = $this->get('biopen.element_import')->importJson($object);        
-        // $this->addFlash('sonata_flash_success', "Les éléments ont été importés avec succès"); 
-        
-        // if ($dataToImport === null)
-        // {
-        //     $this->addFlash('sonata_flash_error', "Un erreur s'est produite lors du téléchargement ou de la lecture du fichier Json");
-        // }
-        // else
-        // {
-        //     $dataSize = count($dataToImport);
-        //     if ($dataSize > 1500)
-        //     {
-        //        $this->get('biopen.async')->callCommand('app:elements:importSource', [$object->getName()]);
-        //        $this->addFlash('sonata_flash_success', "Les " . $dataSize . "éléments sont en cours d'importation. Cela peut prendre plusieurs minutes.");
-        //     }
-        //     else
-        //     {
-        //         $result = $this->get('biopen.element_import')->import($dataToImport, $object, true, true);
-        //         if ($result === null) 
-        //             $this->addFlash('sonata_flash_error', "Un erreur s'est produite lors du chargement du fichier Json. Vérifiez que le fichier est bien valide");
-        //         else {
-        //             $this->addFlash('sonata_flash_success', 'Les ' . $dataSize .' éléments ont été importés avec succès ');            
-        //         }
-        //     }
-        // }            
+        // $this->addFlash('sonata_flash_success', "Les éléments ont été importés avec succès");     
 
         return $this->redirect($this->admin->generateUrl('list'));
     }
