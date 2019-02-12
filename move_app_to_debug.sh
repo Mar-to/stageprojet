bc574bb49f253c853d691eb95a767646bfd08805
Sebastian Castro <90scastro@gmail.com>
2019-02-12 18:36:45 +0100

    Adds script to move from prod to debug environnement

---
+++ move_app_to_debug.sh
@@ -0,0 +1,5 @@
+#!/bash/bin
+
+mv web/app.php web/app.back.php
+mv web/app_dev.php web/app.php
+
---
+++ move_app_to_prod.sh
@@ -0,0 +1,6 @@
+#!/bash/bin
+
+mv web/app.php web/app_dev.php 
+mv web/app.back.php web/app.php 
+
+
