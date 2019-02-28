58ff72e24a95872d28f3141e5415c4d276ef6754
Sebastian Castro <90scastro@gmail.com>
2019-02-28 10:17:28 +0100

    Fixs missing import

--- src/Biopen/GeoDirectoryBundle/Command/CheckExternalSourceToUpdateCommand.php
+++ src/Biopen/GeoDirectoryBundle/Command/CheckExternalSourceToUpdateCommand.php
@@ -5,7 +5,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
-
+use Biopen\GeoDirectoryBundle\Document\ImportState;
 use Biopen\GeoDirectoryBundle\Document\ElementStatus;
 
 use Biopen\SaasBundle\Command\GoGoAbstractCommand;
