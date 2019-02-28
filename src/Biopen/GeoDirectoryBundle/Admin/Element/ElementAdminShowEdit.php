051cb31272c3ee2ece94332dd87d4eb12f5cfbd4
Sebastian Castro <90scastro@gmail.com>
2019-02-28 11:17:40 +0100

    Fixs seeing element in admin

--- src/Biopen/GeoDirectoryBundle/Admin/Element/ElementAdminShowEdit.php
+++ src/Biopen/GeoDirectoryBundle/Admin/Element/ElementAdminShowEdit.php
@@ -23,40 +23,37 @@ class ElementAdminShowEdit extends ElementAdminList
 	{
 	  $formMapper
 	  ->with('Informations générales', array())
-		  ->add('name', 'text')	 
-      ->add('email', 'text', array('required' => false, 'disabled' => true, 'label' => 'Email de contact'))      
-      ->add('userOwnerEmail', 'text', array('required' => false))
-      ->add('images', 'sonata_type_collection', array('by_reference' => true, 'type_options' => array('delete' => true)), 
-         array('edit' => 'inline', 'inline' => 'table'))
-      ->add('stamps', 'sonata_type_model', array(
-            'label' => "Etiquettes",
-            'required' => false,
-            'choices_as_values' => true,
-            'expanded' => false,
-            'multiple' => true,
-        ))
+		  ->add('name', 'text')         
+      ->add('userOwnerEmail', 'text', array('required' => false, 'label' => "Email de l'utilisateur propriétaire de cette fiche"))
+      // ->add('images', 'sonata_type_collection', array('by_reference' => true, 'type_options' => array('delete' => true)), 
+      //    array('edit' => 'inline', 'inline' => 'table'))
+      // ->add('stamps', 'sonata_type_model', array(
+      //       'label' => "Etiquettes",
+      //       'required' => false,
+      //       'choices_as_values' => true,
+      //       'expanded' => false,
+      //       'multiple' => true,
+      //   ))
 		->end();      
 	}	
 
 	protected function configureShowFields(ShowMapper $show)
 	{    
-    
+    $needModeration = $this->subject->getModerationState() != 0;
+    $statusClass = $needModeration ? 'cold-md-6' : 'col-md-12';
     if ($this->subject->isPending())
     {
       $show       
-       ->with('En attente', array('class' => 'col-md-6 col-sm-12'))
-         ->add('currContribution', null, array('template' => '@BiopenAdmin/partials/show_one_contribution.html.twig'))
-       ->end();
+       ->with('En attente', array('class' => $statusClass))
+         ->add('currContribution', null, array('template' => '@BiopenAdmin/partials/show_one_contribution.html.twig'))->end();
     }
     else
     {
-      $show->with('Status', array('class' => 'col-md-6 col-sm-12'))
-         ->add('status', 'choice', [ 'choices'=> $this->statusChoices ])
-      ->end(); 
+      $show->with('Status', array('class' => $statusClass))
+         ->add('status', 'choice', [ 'choices'=> $this->statusChoices ])->end(); 
     }
 
-    if ($this->subject->getModerationState() != 0)
-    {
+    if ($needModeration) {
       $show 
        ->with('Modération', array('class' => 'col-md-6 col-sm-12'))         
         ->add('moderationState', 'choice', [
@@ -66,18 +63,25 @@ class ElementAdminShowEdit extends ElementAdminList
                ])       
         ->add('reports', null, array('template' => '@BiopenAdmin/partials/show_pending_reports.html.twig', 'label' => 'Signalements'))
        ->end();
-    }    
+    }     
     
     $show
-       ->with('Catégorisation', array('class' => 'col-md-6 col-sm-12'))
-	       ->add('optionValues', null, [
-		      	'template' => '@BiopenAdmin/partials/show_option_values.html.twig', 
-		      	'choices' => $this->optionList,
-		      	'label' => 'Catégories'
-		      ])
-       ->end()
+      ->with('Autre infos', array('class' => 'col-md-6 col-sm-12'))
+        ->add('id')
+        ->add('optionValues', null, [
+            'template' => '@BiopenAdmin/partials/show_option_values.html.twig', 
+            'choices' => $this->optionList,
+            'label' => 'Catégories' ])
+        ->add('email', 'text', array('label' => 'Email de contact'))  
+        ->add('images', null, array('template' => '@BiopenAdmin/partials/show_element_images.html.twig'))
+        ->add('randomHash')
+        ->add('oldId', null, array('label' => 'Id dans la base de données importée'))
+        ->add('sourceKey', null, array('label' => 'Source'))
+        ->add('createdAt', 'datetime', array("format" => "d/m/Y à H:i"))
+        ->add('updatedAt', 'datetime', array("format" => "d/m/Y à H:i"))
+      ->end()
 
-       ->with('Localisation', array('class' => 'col-md-6 col-sm-12'))
+      ->with('Localisation', array('class' => 'col-md-6 col-sm-12'))         
         ->add('address.formatedAddress', null, array('label' => 'Adresse complète'))
         ->add('address.streetAddress', null, array('label' => 'Adresse'))
         ->add('address.addressLocality', null, array('label' => 'Ville'))
@@ -85,32 +89,18 @@ class ElementAdminShowEdit extends ElementAdminList
         ->add('address.addressCountry', null, array('label' => 'Pays'))
 	      ->add('geo.latitude')
 	      ->add('geo.longitude')
-       ->end()   
+      ->end()   
 
-       ->with('Champs personnalisés', array('class' => 'col-md-6 col-sm-12'))
+      ->with('Champs personnalisés', array('class' => 'col-md-12 col-sm-12'))
         ->add('data',  null, array('template' => '@BiopenAdmin/partials/show_element_data.html.twig')) 
-       ->end()
-
-       ->with('Champs personnalisés privés', array('class' => 'col-md-6 col-sm-12'))
-        ->add('privateData',  null, array('template' => '@BiopenAdmin/partials/show_element_data.html.twig')) 
-       ->end()
-
-       ->with('Autre infos', array('class' => 'col-md-6 col-sm-12'))
-       	->add('id')
-        ->add('images', null, array('template' => '@BiopenAdmin/partials/show_element_images.html.twig'))
-        ->add('randomHash')
-        ->add('oldId', null, array('label' => 'Id dans la base de données importée'))
-        ->add('sourceKey', null, array('label' => 'Source'))
-	      ->add('createdAt', 'datetime', array("format" => "d/m/Y à H:i"))
-	      ->add('updatedAt', 'datetime', array("format" => "d/m/Y à H:i"))
-       ->end()
+      ->end()       
 
       ->with('Hitorique des contributions', array('class' => 'col-sm-12'))
         ->add('contributions', null, array('template' => '@BiopenAdmin/partials/show_contributions.html.twig'))
       ->end(); 
 
     $show
-      ->with('JSON', array('class' => 'col-md-12'))         
+      ->with('JSON', array('class' => 'col-md-12', 'box_class' => 'box box-default'))         
         ->add('compactJson')
         ->add('baseJson')
         ->add('privateJson')
