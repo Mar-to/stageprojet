bef9b46ab798e432a95f59969727405ff7aa36b0
Sebastian Castro <90scastro@gmail.com>
2019-03-13 15:55:41 +0100

    Simplify ImportAdminController

--- src/Biopen/GeoDirectoryBundle/Controller/Admin/ImportAdminController.php
+++ src/Biopen/GeoDirectoryBundle/Controller/Admin/ImportAdminController.php
@@ -48,80 +48,37 @@ class ImportAdminController extends Controller
         ]);    
     }
 
-
     // This method is just an overwrite of the SonataAdminCRUDController for calling the executeImport once the document is created
     public function createAction()
     {
         $request = $this->getRequest();
-        // the key used to lookup the template
-        $templateKey = 'edit';
-
         $this->admin->checkAccess('create');
-
-        $class = new \ReflectionClass($this->admin->hasActiveSubClass() ? $this->admin->getActiveSubClass() : $this->admin->getClass());
-
-        if ($class->isAbstract()) {
-            return $this->render(
-                'SonataAdminBundle:CRUD:select_subclass.html.twig',
-                array(
-                    'base_template' => $this->getBaseTemplate(),
-                    'admin' => $this->admin,
-                    'action' => 'create',
-                ),
-                null,
-                $request
-            );
-        }
-
         $object = $this->admin->getNewInstance();
-
-        $preResponse = $this->preCreate($request, $object);
-        if ($preResponse !== null) {
-            return $preResponse;
-        }
-
         $this->admin->setSubject($object);
 
-        /** @var $form \Symfony\Component\Form\Form */
         $form = $this->admin->getForm();
         $form->setData($object);
         $form->handleRequest($request);
 
         if ($form->isSubmitted()) {
             $isFormValid = $form->isValid();
-
             // persist if the form was valid and if in preview mode the preview was approved
             if ($isFormValid) {
-                $this->admin->checkAccess('create', $object);
-
                 try {
                     $object = $this->admin->create($object);   
                     return $this->executeImport($object);
-
                 } catch (ModelManagerException $e) {
                     $this->handleModelManagerException($e);
-
                     $isFormValid = false;
                 }
             }
-
-            // show an error message if the form failed validation
-            if (!$isFormValid) {
-                if (!$this->isXmlHttpRequest()) {
-                    $this->addFlash(
-                        'sonata_flash_error',
-                        $this->trans(
-                            'flash_create_error',
-                            array('%name%' => $this->escapeHtml($this->admin->toString($object))),
-                            'SonataAdminBundle'
-                        )
-                    );
-                }
+            if (!$isFormValid) { // show an error message if the form failed validation
+                $text = $this->trans('flash_create_error', array('%name%' => $this->escapeHtml($this->admin->toString($object))), 'SonataAdminBundle');
+                $this->addFlash('sonata_flash_error', $text);
             }
         }
 
         $view = $form->createView();
-
         // set the theme for the current Admin Form
         $this->get('twig')->getExtension('form')->renderer->setTheme($view, $this->admin->getFormTheme());
 
