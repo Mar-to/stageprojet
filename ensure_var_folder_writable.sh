3d6b92bd682bdc7079507376cbc6cf29ca22c89d
Sebastian Castro <sebastian.castro@protonmail.com>
2019-10-12 09:07:39 +0200

    Add dummy script to test

---
+++ ensure_var_folder_writable.sh
@@ -0,0 +1,15 @@
+chmod 777 -R var/
+sleep 60
+chmod 777 -R var/
+sleep 600
+chmod 777 -R var/
+sleep 3600
+chmod 777 -R var/
+sleep 3600
+chmod 777 -R var/
+sleep 30000
+chmod 777 -R var/
+sleep 30000
+chmod 777 -R var/
+sleep 30000
+chmod 777 -R var/
