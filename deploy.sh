5e8d16750020a8ca98b482235ccaef58cc12829c
Sebastian Castro <sebastian.castro@protonmail.com>
2019-12-04 20:58:20 +0100

    var cache relative folder

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,9 @@ gulp production
 php bin/console db:migrate
 sh cache_clear.sh
 
-sleep 10 && chmod 777 -R /var/www/gogocarto/var/ &
-sleep 60 && chmod 777 -R /var/www/gogocarto/var/ &
-sleep 120 && chmod 777 -R /var/www/gogocarto/var/ &
-sleep 300 && chmod 777 -R /var/www/gogocarto/var/ &
-sleep 600 && chmod 777 -R /var/www/gogocarto/var/ &
-sleep 2000 && chmod 777 -R /var/www/gogocarto/var/ &
+sleep 10 && chmod 777 -R var/ &
+sleep 60 && chmod 777 -R var/ &
+sleep 120 && chmod 777 -R var/ &
+sleep 300 && chmod 777 -R var/ &
+sleep 600 && chmod 777 -R var/ &
+sleep 2000 && chmod 777 -R var/ &
