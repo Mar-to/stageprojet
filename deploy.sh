fc2ea1d28a11a34dab0774b863f192325ff62a00
Sebastian Castro <sebastian.castro@protonmail.com>
2019-12-04 18:22:33 +0100

    Try fix cache folder not writable after deployment

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,10 @@ gulp build
 gulp production
 php bin/console db:migrate
 sh cache_clear.sh
-sleep 5
-chmod 777 -R var/
-sleep 10
-chmod 777 -R var/
-chmod 777 -R /var/www/gogocarto/var/
+
+sleep 10 && chmod 777 -R /var/www/gogocarto/var/ &
+sleep 60 && chmod 777 -R /var/www/gogocarto/var/ &
+sleep 120 && chmod 777 -R /var/www/gogocarto/var/ &
+sleep 300 && chmod 777 -R /var/www/gogocarto/var/ &
+sleep 600 && chmod 777 -R /var/www/gogocarto/var/ &
+sleep 2000 && chmod 777 -R /var/www/gogocarto/var/ &
