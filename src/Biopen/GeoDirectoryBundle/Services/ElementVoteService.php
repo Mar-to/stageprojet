c9190c102173e882f11d359e6419644a143be785
Sebastian Castro <sebastian.castro@protonmail.com>
2020-01-17 12:10:27 +0100

    Fix check vote with null currContrib

--- src/Biopen/GeoDirectoryBundle/Services/ElementVoteService.php
+++ src/Biopen/GeoDirectoryBundle/Services/ElementVoteService.php
@@ -9,7 +9,7 @@
  * @license    MIT License
  * @Last Modified time: 2018-02-11 13:06:48
  */
- 
+
 
 namespace Biopen\GeoDirectoryBundle\Services;
 
@@ -24,7 +24,7 @@ use Biopen\GeoDirectoryBundle\Services\ElementPendingService;
 use Biopen\GeoDirectoryBundle\Services\ValidationType;
 
 class ElementVoteService
-{	
+{
 	/**
      * Constructor
      */
@@ -32,7 +32,7 @@ class ElementVoteService
     {
         $this->em = $documentManager;
         $this->user = $securityContext->getToken() ? $securityContext->getToken()->getUser() : null;
-        $this->confService = $confService; 
+        $this->confService = $confService;
         $this->securityContext = $securityContext;
         $this->elementPendingService = $elementPendingService;
     }
@@ -43,7 +43,7 @@ class ElementVoteService
         // Check user don't vote for his own creation
         if ($element->isLastContributorEqualsTo($this->user, $userEmail))
                 return "Voyons voyons, vous ne comptiez quand même pas voter pour votre propre contribution si ? Laissez-en un peu pour les autres !</br>
-                        Attention les petits malins, si vous utilisez une autre de vos adresse perso on le verra aussi ! "; 
+                        Attention les petits malins, si vous utilisez une autre de vos adresse perso on le verra aussi ! ";
 
         $hasAlreadyVoted = false;
 
@@ -51,16 +51,16 @@ class ElementVoteService
         {
             $procedureCompleteMessage = $this->handleVoteProcedureComplete($element, ValidationType::Admin, $voteValue >= 1, $comment);
         }
-        else 
+        else
         {
             // CHECK USER HASN'T ALREADY VOTED
-            $currentVotes = $element->getVotes();            
+            $currentVotes = $element->getVotes();
             // if user is anonymous no need to check
             if ($userEmail || $this->user)
             {
-                foreach ($currentVotes as $oldVote) 
+                foreach ($currentVotes as $oldVote)
                 {
-                    if ($oldVote->isMadeBy($this->user, $userEmail)) 
+                    if ($oldVote->isMadeBy($this->user, $userEmail))
                     {
                         $hasAlreadyVoted = true;
                         $vote = $oldVote;
@@ -68,8 +68,8 @@ class ElementVoteService
                 }
             }
 
-            if (!$hasAlreadyVoted) $vote = new UserInteractionVote();       
-            
+            if (!$hasAlreadyVoted) $vote = new UserInteractionVote();
+
             $vote->setValue($voteValue);
             $vote->setElement($element);
             $vote->updateUserInformation($this->securityContext, $userEmail);
@@ -97,13 +97,15 @@ class ElementVoteService
     *   - Enough votes to change status
     *   - Not too much opposites votes
     *   - Waiting for minimum days after contribution to validate or invalidate
-    * 
+    *
     * If an element is pending for too long, it's set in Moderation
     *
     * This action is called when user vote, and with a CRON job every days
     */
     public function checkVotes($element)
     {
+        if (!$element->getCurrContribution()) return;
+
         $currentVotes = $element->getVotes();
         $nbrePositiveVote = 0;
         $nbreNegativeVote = 0;
@@ -111,7 +113,7 @@ class ElementVoteService
         $diffDate = time() - $element->getCurrContribution()->getCreatedAt()->getTimestamp();
         $daysFromContribution = floor( $diffDate / (60 * 60 * 24));
 
-        foreach ($currentVotes as $key => $vote) 
+        foreach ($currentVotes as $key => $vote)
         {
            $vote->getValue() >= 0 ? $nbrePositiveVote++ : $nbreNegativeVote++;
         }
@@ -123,22 +125,22 @@ class ElementVoteService
 
         if ($nbrePositiveVote >= $minVotesToChangeStatus)
         {
-            if ($nbreNegativeVote <= $maxOppositeVoteTolerated) 
+            if ($nbreNegativeVote <= $maxOppositeVoteTolerated)
             {
                 if ($enoughDays || $nbrePositiveVote >= $minVotesToForceChangeStatus) return $this->handleVoteProcedureComplete($element, ValidationType::Collaborative, true);
             }
-            else 
+            else
             {
                 $element->setModerationState(ModerationState::VotesConflicts);
             }
         }
         else if ($nbreNegativeVote >= $minVotesToChangeStatus)
         {
-            if ($nbrePositiveVote <= $maxOppositeVoteTolerated) 
+            if ($nbrePositiveVote <= $maxOppositeVoteTolerated)
             {
                 if ($enoughDays || $nbreNegativeVote >= $minVotesToForceChangeStatus) return $this->handleVoteProcedureComplete($element, ValidationType::Collaborative, false);
             }
-            else 
+            else
             {
                 $element->setModerationState(ModerationState::VotesConflicts);
             }
@@ -150,29 +152,29 @@ class ElementVoteService
     }
 
     private function handleVoteProcedureComplete($element, $voteType, $positiveVote, $customMessage = '')
-    {        
+    {
         // in case of procedure complete directly after a userInteraction, we send a message back to the user
         $flashMessage = '';
         $elDisplayName = $this->confService->getConfig()->getElementDisplayNameDefinite();
 
         if ($element->getStatus() == ElementStatus::PendingAdd)
         {
-            if ($voteType == ValidationType::Collaborative) 
-                $flashMessage = $positiveVote ? "Félicitations, " . $elDisplayName . " a reçu assez de vote pour être validé !" 
+            if ($voteType == ValidationType::Collaborative)
+                $flashMessage = $positiveVote ? "Félicitations, " . $elDisplayName . " a reçu assez de vote pour être validé !"
                                       : ucwords($elDisplayName) . " a reçu suffisamment de votes négatifs, il va être supprimé.";
-            else if ($voteType == ValidationType::Admin)    
-                $flashMessage = $positiveVote ? ucwords($elDisplayName) . " a bien été validé" : ucwords($elDisplayName) . " a bien été refusé";           
+            else if ($voteType == ValidationType::Admin)
+                $flashMessage = $positiveVote ? ucwords($elDisplayName) . " a bien été validé" : ucwords($elDisplayName) . " a bien été refusé";
         }
         else if ($element->getStatus() == ElementStatus::PendingModification)
-        {            
+        {
             if ($positiveVote)
                 $flashMessage = $voteType == ValidationType::Admin ? "Les modifications ont bien été acceptées" : "Félicitations, les modifications ont reçues assez de vote pour être validées !";
             else
-                $flashMessage = $voteType == ValidationType::Admin ? "Les modifications ont bien été refusées" : "La proposition de modification a reçu suffisamment de votes négatifs, elle est annulée.";      
-        }      
+                $flashMessage = $voteType == ValidationType::Admin ? "Les modifications ont bien été refusées" : "La proposition de modification a reçu suffisamment de votes négatifs, elle est annulée.";
+        }
 
         // Handle validation or refusal with dedicate service
-        $this->elementPendingService->resolve($element, $positiveVote, $voteType, $customMessage);  
+        $this->elementPendingService->resolve($element, $positiveVote, $voteType, $customMessage);
 
         return $flashMessage;
     }
