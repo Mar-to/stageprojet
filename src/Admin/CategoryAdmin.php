656622e12e0d44576356fef68555149264cae12e
Sebastian Castro <sebastian.castro@protonmail.com>
2020-05-23 15:25:11 +0200

    Category parent allow to choose empty value

--- src/Admin/CategoryAdmin.php
+++ src/Admin/CategoryAdmin.php
@@ -51,24 +51,25 @@ class CategoryAdmin extends AbstractAdmin
         }
 
         $formMapper
-      ->with('Paramètres principaux', ['class' => 'col-xs-12 col-md-6'])
+          ->with('Paramètres principaux', ['class' => 'col-xs-12 col-md-6'])
             ->add('name', null, ['required' => true, 'label' => 'Nom du groupe'])
             ->add('pickingOptionText', null, ['required' => true, 'label' => 'Texte à afficher dans le formulaire : Choisissez ....'])
             ->add('parent', ModelType::class, [
                 'class' => 'App\Document\Option',
                 'required' => false,
                 'query' => $parentQuery,
+                'placeholder' => 'Racine',
                 'label' => 'Catégorie parente', ], ['admin_code' => 'admin.option_hidden'])
-        ->add('isMandatory', null, ['required' => false, 'label' => 'Choix obligatoire', 'label_attr' => ['title' => 'Une catégorie de ce groupe doit être obligatoirement selectionnée']])
-        ->add('singleOption', null, ['required' => false, 'label' => 'Choix unique', 'label_attr' => ['title' => 'Une seule catégorie est selectionnable à la fois']])
-        ->add('enableDescription', null, ['required' => false, 'label' => 'Activer la description des catégories', 'label_attr' => ['title' => 'On pourra renseigner un texte pour décrire chaque catégorie. Par example, pour un catégorie Agriculture, on pourrait ajouter comme texte "Maraîchage, produits transformés..."']])
-        ->end()
-        ->with('Paramètres secondaires d\'affichage', ['class' => 'col-xs-12 col-md-6', 'box_class' => 'box'])
-         ->add('nameShort', null, ['required' => false, 'label' => 'Nom (version courte)', 'label_attr' => ['title' => 'La version courte est utilisée dans le menu, car souvent on manque de place']])
-         ->add('index', null, ['required' => false, 'label' => 'Position'])
-         ->add('showExpanded', null, ['required' => false, 'label' => 'En position intiale afficher les catégories (sinon il faudra cliquer pour les afficher)'])
-               ->add('unexpandable', null, ['required' => false, 'label' => 'Ne pas pouvoir reduire ce groupe de catégories', 'label_attr' => ['title' => 'Dans le menu, les catégories de ce groupe seront toujours affichées']])
-         ->add('displaySuboptionsInline', null, ['required' => false, 'label' => 'Afficher les sous catégories sur une seule ligne'])
+            ->add('isMandatory', null, ['required' => false, 'label' => 'Choix obligatoire', 'label_attr' => ['title' => 'Une catégorie de ce groupe doit être obligatoirement selectionnée']])
+            ->add('singleOption', null, ['required' => false, 'label' => 'Choix unique', 'label_attr' => ['title' => 'Une seule catégorie est selectionnable à la fois']])
+            ->add('enableDescription', null, ['required' => false, 'label' => 'Activer la description des catégories', 'label_attr' => ['title' => 'On pourra renseigner un texte pour décrire chaque catégorie. Par example, pour un catégorie Agriculture, on pourrait ajouter comme texte "Maraîchage, produits transformés..."']])
+            ->end()
+            ->with('Paramètres secondaires d\'affichage', ['class' => 'col-xs-12 col-md-6', 'box_class' => 'box'])
+             ->add('nameShort', null, ['required' => false, 'label' => 'Nom (version courte)', 'label_attr' => ['title' => 'La version courte est utilisée dans le menu, car souvent on manque de place']])
+             ->add('index', null, ['required' => false, 'label' => 'Position'])
+             ->add('showExpanded', null, ['required' => false, 'label' => 'En position intiale afficher les catégories (sinon il faudra cliquer pour les afficher)'])
+                   ->add('unexpandable', null, ['required' => false, 'label' => 'Ne pas pouvoir reduire ce groupe de catégories', 'label_attr' => ['title' => 'Dans le menu, les catégories de ce groupe seront toujours affichées']])
+             ->add('displaySuboptionsInline', null, ['required' => false, 'label' => 'Afficher les sous catégories sur une seule ligne'])
         ->end()
       ->with('Afficher ce groupe', ['class' => 'col-md-6', 'box_class' => 'box'])
          ->add('displayInMenu', null, ['required' => false, 'label' => 'Dans le menu', 'label_attr' => ['title' => 'Le nom du groupe ne sera pas affiché, mais les catégories le seront']])
