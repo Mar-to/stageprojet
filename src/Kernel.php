d71bdce57dd8ae399572b7f9b9bb5b53ff303a76
Sebastian Castro <sebastian.castro@protonmail.com>
2021-01-05 13:51:17 +0100

    config: extract parameters from services.yaml

--- src/Kernel.php
+++ src/Kernel.php
@@ -38,6 +38,7 @@ class Kernel extends BaseKernel
         $container->setParameter('container.dumper.inline_factories', true);
         $confDir = $this->getProjectDir().'/config';
 
+        $loader->load($confDir.'/parameters'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{packages}/'.$this->environment.'/*'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
