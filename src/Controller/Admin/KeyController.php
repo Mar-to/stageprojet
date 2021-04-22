7e5ad46d1ceac52fc0a9f26b8f0b9078c3d5d2a1
Tom MM <82344914+Sorhen-Hi@users.noreply.github.com>
2021-04-22 11:38:04 +0200

    Create KeyController.php

---
+++ src/Controller/Admin/KeyController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as Controller;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class KeyController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('admin/pages/key.html.twig');
+    }
+
+    public function generateAction(SessionInterface $session)
+    {
+        $keyLength = 10;
+        $key = substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($keyLength/strlen($x)) )),1,$keyLength);
+
+        $session->getFlashBag()->add('success', ' La clé générer est : '.$key);
+
+        return $this->redirect($this->generateUrl('gogo_core_key'));
+    }
+}
