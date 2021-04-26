0d15c7cea47f0cbb789d4e695980a427f016fedf
Tom MM <82344914+Sorhen-Hi@users.noreply.github.com>
2021-04-26 17:50:52 +0200

    Update KeyController.php

--- src/Controller/Admin/KeyController.php
+++ src/Controller/Admin/KeyController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as Controller;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Doctrine\ODM\MongoDB\DocumentManager;
+use App\Document\CodeInvitation;
 
 class KeyController extends Controller
 {
@@ -12,11 +14,20 @@ class KeyController extends Controller
         return $this->render('admin/pages/key.html.twig');
     }
 
-    public function generateAction(SessionInterface $session)
+    public function generateAction(SessionInterface $session, DocumentManager $dm)
     {
         $keyLength = 10;
         $key = substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($keyLength/strlen($x)) )),1,$keyLength);
 
+
+        //ObjectManager $manager
+        $new_CodeInvitation = new CodeInvitation();
+        $new_CodeInvitation->setCode($key);
+        $dm->persist($new_CodeInvitation);
+
+        // we trigger saving of all abouts
+        $dm->flush();
+        
         $session->getFlashBag()->add('success', ' La clé générée est : '.$key);
 
         return $this->redirect($this->generateUrl('gogo_core_key'));
